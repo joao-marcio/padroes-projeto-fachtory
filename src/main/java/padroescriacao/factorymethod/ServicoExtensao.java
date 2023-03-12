38a223bff276bc3c524f17b4c7ff346afe4f16db
JoaoMarcioRamos <jmrsgua@gmail.com>
2023-03-12 20:53:12 -0300

    Alteração código

---
+++ src/main/java/padroescriacao/factorymethod/ServicoExtensao.java
@@ -0,0 +1,12 @@
+package padroescriacao.factorymethod;
+
+public class ServicoExtensao  {
+
+    public String executar() {
+        return "Aluno inscrito no processo";
+    }
+
+    public String cancelar() {
+        return "Aluno não inscrito no processo";
+    }
+}
